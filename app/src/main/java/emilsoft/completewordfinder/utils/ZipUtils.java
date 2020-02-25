9986a123de36f846955681a74c16ac7b3119a838
e.osterhed <e.osterhed@campus.unimib.it>
2020-02-25 14:58:24 +0100

    Add ZipUtils and dictionaries' zip files

---
+++ app/src/main/java/emilsoft/completewordfinder/utils/ZipUtils.java
@@ -0,0 +1,46 @@
+package emilsoft.completewordfinder.utils;
+
+import android.util.Log;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+import emilsoft.completewordfinder.MainActivity;
+
+public class ZipUtils {
+
+    private static int DEFAULT_BUFFER_SIZE = 65536;
+
+    public static void unzip(File zipFile, File targetDirectory) {
+        try (FileInputStream fis = new FileInputStream(zipFile)) {
+            try (BufferedInputStream bis = new BufferedInputStream(fis)) {
+                try (ZipInputStream zis = new ZipInputStream(bis)) {
+                    ZipEntry ze;
+                    int count;
+                    byte[] buffer = new byte[DEFAULT_BUFFER_SIZE];
+                    while ((ze = zis.getNextEntry()) != null) {
+                        File file = new File(targetDirectory, ze.getName());
+                        File dir = ze.isDirectory() ? file : file.getParentFile();
+                        if (!dir.isDirectory() && !dir.mkdirs())
+                            throw new FileNotFoundException("Failed to ensure directory: "+dir.getAbsolutePath());
+                        if (ze.isDirectory())
+                            continue;
+                        try (FileOutputStream fout = new FileOutputStream(file)) {
+                            while ((count = zis.read(buffer)) != -1)
+                                fout.write(buffer, 0, count);
+                        }
+                    }
+                }
+            }
+        } catch (IOException | NullPointerException ex) {
+            Log.v(MainActivity.TAG, "Zip not opened.\n"+ex.getMessage());
+        }
+    }
+
+}
